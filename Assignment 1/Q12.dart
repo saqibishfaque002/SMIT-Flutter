d4e678d58596380c034ced6a62e42da87b6308e1
saqibishfaque002 <saqibishfaquekhi@gmail.com>
2023-06-16 14:20:33 +0500

    Add Class 3

--- Assignment 1/Q12.dart
+++
@@ -1,5 +0,0 @@
-void main() {
-double celsius = 50;
-var fahrenheit = (celsius * 9 / 5) + 32;
-print('$celsius degrees Celsius is equal to $fahrenheit degrees Fahrenheit.');
-}
---
+++ class3.dart
@@ -0,0 +1,4 @@
+/*
+First time setup:
+
+*/
