0f909c69ee5d781dc44cb5cdbdb4b21156f64283
saqibishfaque002 <saqibishfaquekhi@gmail.com>
2023-06-16 14:47:37 +0500

    Changes

--- class3.dart
+++ class3.dart
@@ -6,6 +6,10 @@ git config --global user.email "email"
 For first time project upload:
 git init
 git add remote origin "Repo link"
+git add . OR git add filename
+git commit -m "message"
+git add remote origin "Repo link"
+git push -u origin master
 
 For project update:
 */
