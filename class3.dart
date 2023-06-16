f74aabda857d5077050256002891563a708ff693
saqibishfaque002 <saqibishfaquekhi@gmail.com>
2023-06-16 14:43:03 +0500

    Add Code Pushing Detail

--- class3.dart
+++ class3.dart
@@ -4,4 +4,8 @@ git config --global user.name "username"
 git config --global user.email "email"
 
 For first time project upload:
+git init
+git add remote origin "Repo link"
+
+For project update:
 */
