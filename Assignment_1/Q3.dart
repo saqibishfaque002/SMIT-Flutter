56c170e6539662665a14a23054cfecb4b264baef
saqibishfaque002 <saqibishfaquekhi@gmail.com>
2023-06-17 11:26:52 +0500

    Add Question Q3

--- Assignment_1/Q3.dart
+++ Assignment_1/Q3.dart
@@ -1,3 +1,10 @@
+/* 
+Q.3: A student will not be allowed to sit in exam if his/her attendance is less than 75%. Create integer variables and assign value:
+Number of classes held = 16,
+Number of classes attended = 10,
+and print percentage of class attended.
+Is student is allowed to sit in exam or not?
+*/
 void main() {
   // Create two integer variables
   int number_of_classes_held = 16;
