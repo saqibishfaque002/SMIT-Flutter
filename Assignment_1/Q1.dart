ade182faf1de55b823768eb685b11fba367caa84
saqibishfaque002 <saqibishfaquekhi@gmail.com>
2023-06-17 11:17:23 +0500

    Add Question Text

--- Assignment_1/Q1.dart
+++ Assignment_1/Q1.dart
@@ -1,3 +1,8 @@
+/*
+Q.1: Create two integer variables length and breadth and assign values then check if they are square values or rectangle values.
+ie: if both values are equal then it's square otherwise rectangle.
+*/
+
 void main() {
 // Create two integer variables
   int length = 10;
