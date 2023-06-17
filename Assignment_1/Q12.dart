eccbd78b1eb96414a7b9d03ade4b4f3536ddb4bc
saqibishfaque002 <saqibishfaquekhi@gmail.com>
2023-06-17 11:43:02 +0500

    Add Question Q12

--- Assignment_1/Q12.dart
+++ Assignment_1/Q12.dart
@@ -1,3 +1,7 @@
+/* 
+Q12: Write a program to convert Celsius  to Fahrenheit   .
+i.e: Temperature in degrees Fahrenheit (°F) = (Temperature in degrees Celsius (°C) * 9/5) + 32
+*/
 void main() {
 double celsius = 50;
 var fahrenheit = (celsius * 9 / 5) + 32;
