ba899852fbf401f19c63454142869f14bad44a58
saqibishfaque002 <saqibishfaquekhi@gmail.com>
2023-06-17 11:20:31 +0500

    Add Question Text

--- Assignment_1/Q2.dart
+++ Assignment_1/Q2.dart
@@ -1,3 +1,6 @@
+/*
+Q.2: Take two variables and store age then using if/else condition to determine oldest and youngest among them.
+*/
 void main() {
 // declare two variables to store age
   int age1 = 10;
