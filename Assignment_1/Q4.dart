e45c78a08913b5000e93a90e57265fb06b607cc2
saqibishfaque002 <saqibishfaquekhi@gmail.com>
2023-06-17 11:34:27 +0500

    Add Question Q4

--- Assignment_1/Q4.dart
+++ Assignment_1/Q4.dart
@@ -1,3 +1,8 @@
+/* 
+Q.4: Create integer variable assign any year to it and check if a year is leap year or not.
+If a year is divisible by 4 then it is leap year but if the year is century year like 2000, 1900, 2100 then it must be divisible by 400.
+i.e: Use % ( modulus ) operator.
+*/
 void main() {
   // Create an integer variable
   int year = 2000;
