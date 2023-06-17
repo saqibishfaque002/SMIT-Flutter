b868034f4fa181ee5adc349ff6703663ef363014
saqibishfaque002 <saqibishfaquekhi@gmail.com>
2023-06-17 11:38:36 +0500

    Add Question Q7

--- Assignment_1/Q7.dart
+++ Assignment_1/Q7.dart
@@ -1,3 +1,23 @@
+/*
+Q.7: Write a program to calculate and print the Electricity bill of a given customer. Create variable for customer id, name, unit consumed by the user, bill_amount and print the total amount the customer needs to pay. The charge are as follow :
+
+Unit    Charge/unit
+upto 199    @1.20
+200 and above but less than 400    @1.50
+400 and above but less than 600    @1.80
+600 and above             @2.00;
+
+Test Data :
+id: 1001
+name: James
+units: 800
+Expected Output :
+Customer IDNO :1001
+Customer Name :James
+unit Consumed :800
+Amount Charges @Rs. 2.00 per unit : 1600.00
+Net Bill Amount : 1600.00
+*/
 void main() {
   // Define variables
   int customer_id = 123456;
