cbb0aa904e0d56539ca6e86d7eee48eafeb04c7f
saqibishfaque002 <saqibishfaquekhi@gmail.com>
2023-06-17 11:40:36 +0500

    Add Question Q9

--- Assignment_1/Q9.dart
+++ Assignment_1/Q9.dart
@@ -1,5 +1,8 @@
+/* 
+ Q9: Check if the number is even or odd, If number is even then check if this is divisible by 5 
+ or not & if number is odd then check if this is divisible by 7 or not. 
+*/
 // ignore_for_file: unused_local_variable
-
 void main() {
   // Get the number from the user
   int number = 10;
