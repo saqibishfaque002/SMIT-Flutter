78c46cf20b53990fe73606abc442170d759a2a79
saqibishfaque002 <saqibishfaquekhi@gmail.com>
2023-06-17 11:36:09 +0500

    Add Question Q5

--- Assignment_1/Q5.dart
+++ Assignment_1/Q5.dart
@@ -1,3 +1,14 @@
+/*
+Q.5  Write a program to read temperature in centigrade and display a suitable message according to temperature:
+You have num variable temperature = 42;
+Now print the message according to temperature:
+temp < 0 then Freezing weather
+temp 0-10 then Very Cold weather
+temp 10-20 then Cold weather
+temp 20-30 then Normal in Temp
+temp 30-40 then Its Hot
+temp >=40 then Its Very Hot
+*/
 void main() {
   // Create a variable to store the temperature
   int temperature = 42;
