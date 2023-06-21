461adf00e9b71aee7681e9040ff37325987c1954
Saqib Ishfaque <110449595+saqibishfaque002@users.noreply.github.com>
2023-06-21 11:42:00 +0500

    Delete Assingment_2 directory

--- Assingment_2/Q1.dart
+++
@@ -1,8 +0,0 @@
-void main() {
-  List<String> names = ['Saqib', 'Ammar', 'Bilal', 'Sheraaz'];
-
-  // Printing all names using the List method
-  for (String name in names) {
-    print(name);
-  }
-}
