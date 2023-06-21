273eea2ce5e5a0df3c315e7c01eb4bbd49fdfc6f
saqibishfaque002 <saqibishfaquekhi@gmail.com>
2023-06-21 11:38:58 +0500

    Add Assignment 2

---
+++ Assingment_2/Q1.dart
@@ -0,0 +1,8 @@
+void main() {
+  List<String> names = ['Saqib', 'Ammar', 'Bilal', 'Sheraaz'];
+
+  // Printing all names using the List method
+  for (String name in names) {
+    print(name);
+  }
+}
