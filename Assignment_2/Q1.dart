66ef9d24e30bc354dee395838ec83e105602e73e
saqibishfaque002 <saqibishfaquekhi@gmail.com>
2023-06-21 11:50:38 +0500

    Add Question & Answer

---
+++ Assignment_2/Q1.dart
@@ -0,0 +1,9 @@
+// Q.1: Create a list of names and print all names using the List method.
+void main() {
+  List<String> names = ['Saqib', 'Ammar', 'Bilal', 'Sheraaz'];
+
+  // Printing all names using the List method
+  for (String name in names) {
+    print(name);
+  }
+}
