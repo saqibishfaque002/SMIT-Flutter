c4706e23192241cfcf11bfb42915e5052701560f
ibtiali <ibtiwebdev93@gmail.com>
2023-06-24 13:33:18 +0500

    Changed Q21

---
+++ Assignment_2/Q21.dart
@@ -0,0 +1,20 @@
+/*
+Q.21: Given a map representing a user with keys "name", "isAdmin", and "isActive", write Dart code to check if the user is an active admin. 
+If the user is both an admin and active, print "Active admin", otherwise print "Not an active admin".
+*/
+void main() {
+  Map<String, dynamic> user = {
+    'name': 'Saqib Ishfaque',
+    'isAdmin': true,
+    'isActive': true,
+  };
+
+  bool isAdmin = user['isAdmin'];
+  bool isActive = user['isActive'];
+
+  if (isAdmin && isActive) {
+    print("Active admin");
+  } else {
+    print("Not an active admin");
+  }
+}
