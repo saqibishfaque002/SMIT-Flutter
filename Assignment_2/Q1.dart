5bb82c597b767ea38d98e009c223ab40da6e48bb
ibtiali <ibtiwebdev93@gmail.com>
2023-06-24 11:14:32 +0500

    Changed Q1

--- Assignment_2/Q1.dart
+++ Assignment_2/Q1.dart
@@ -1,6 +1,10 @@
 // Q.1: Create a list of names and print all names using the List method.
 void main() {
-  List<String> names = ['Alice', 'Bob', 'Charlie', 'Dave', 'Eve'];
+  // Create a list of names
+  List<String> names = ["Saqib", "Alice", "Hassan", "Bilal", "Hannan"];
 
-  names.forEach((name) => print(name));
+  // Print all names using forEach method
+  names.forEach((name) {
+    print(name);
+  });
 }
