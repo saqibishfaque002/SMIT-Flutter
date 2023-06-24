32a9d28590c4b2cdeb0202aa32d08d58ad063a66
ibtiali <ibtiwebdev93@gmail.com>
2023-06-24 13:07:54 +0500

    Changed Q8

--- Assignment_2/Q8.dart
+++ Assignment_2/Q8.dart
@@ -1,17 +1,40 @@
-// Q.8: From Q3 write a program that prints the days that start with the letter "S".
+/*
+Q.8: remove all false values from below list by using removeWhere or retainWhere property.
+
+List<Map<String, bool>> usersEligibility = [
+{'name': 'John', 'eligible': true},
+{'name': 'Alice', 'eligible': false},
+{'name': 'Mike', 'eligible': true},
+{'name': 'Sarah', 'eligible': true},
+{'name': 'Tom', 'eligible': false},
+];
+*/
 void main() {
-  List<String> daysOfWeek = [
-    'Sunday',
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday'
+  List<Map<String, dynamic>> usersEligibility1 = [
+    {'name': 'John', 'eligible': true},
+    {'name': 'Alice', 'eligible': false},
+    {'name': 'Mike', 'eligible': true},
+    {'name': 'Sarah', 'eligible': true},
+    {'name': 'Tom', 'eligible': false},
   ];
 
-  List<String> daysStartingWithS =
-      daysOfWeek.where((day) => day.startsWith('S')).toList();
+  // Remove map entries with 'false' values for 'eligible' key
+  usersEligibility1.removeWhere((user) => user['eligible'] == false);
 
-  daysStartingWithS.forEach((day) => print(day));
+  // Print the updated usersEligibility list
+  // print(usersEligibility1);
+
+  List<Map<String, dynamic>> usersEligibility = [
+    {'name': 'John', 'eligible': true},
+    {'name': 'Alice', 'eligible': false},
+    {'name': 'Mike', 'eligible': true},
+    {'name': 'Sarah', 'eligible': true},
+    {'name': 'Tom', 'eligible': false},
+  ];
+
+  // Remove map entries with 'false' values for 'eligible' key
+  usersEligibility.retainWhere((user) => user['eligible'] == true);
+
+  // Print the updated usersEligibility list
+  print(usersEligibility);
 }
