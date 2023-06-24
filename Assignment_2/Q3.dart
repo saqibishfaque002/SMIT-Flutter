c4bf12315a38235e17deeba4fa6ed4d605b20b71
ibtiali <ibtiwebdev93@gmail.com>
2023-06-24 11:19:37 +0500

    Changed Q3

--- Assignment_2/Q3.dart
+++ Assignment_2/Q3.dart
@@ -1,18 +1,19 @@
 // Q.3: Create a list of Days and remove one by one from the end of list.
 void main() {
+  // Create a list of days
   List<String> days = [
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday',
-    'Sunday'
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
   ];
 
-  // Removing days one by one from the end of the list
+  // Remove days one by one from the end of the list
   while (days.isNotEmpty) {
     String removedDay = days.removeLast();
-    print('Removed day: $removedDay');
+    print("Removed day: $removedDay");
   }
 }
