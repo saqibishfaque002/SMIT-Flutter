0772273a6f524ae06301e1ac8ffadd70d2175e97
saqibishfaque002 <saqibishfaquekhi@gmail.com>
2023-07-05 11:59:07 +0500

    Changes

--- Assignment_3/Q24.dart
+++ Assignment_3/Q24.dart
@@ -3,7 +3,7 @@ Q25, Write a program that takes a list of integers as input and returns a new
 list containing only the prime numbers from the original list. Implement
 the solution using a for loop and logical operations.
 Input: [4, 7, 10, 13, 16, 19, 22, 25, 28, 31]
-Output: [7, 13, 19, 31] 
+Output: [7, 13, 19, 31]  
 */
 void main() {
   List<int> numbers = [5, 7, 13, 16, 19, 22];
