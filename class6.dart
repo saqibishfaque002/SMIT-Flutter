cf0d444ee95657fc9bc25dd716883da6bfbc83dd
ibtiali <ibtiwebdev93@gmail.com>
2023-07-05 11:25:39 +0500

    Add Assignment

---
+++ class6.dart
@@ -0,0 +1,17 @@
+void main() {
+  // pre increment ++variable
+  // int abc = 1;
+  // var a = ++abc;
+  // print(a);
+
+  // post increment variable++
+  // int abc = 1;
+  // var a = abc++;
+  // print(a);
+
+  // Loops
+  int number = 2;
+  for (var i = 1; i <= 10; i++) {
+    print("$number * $i = ${number * i}");
+  }
+}
